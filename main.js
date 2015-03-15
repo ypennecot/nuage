7e5d1a007ea5d7b76edd74f2f4cb782d340331ea
yannick <yannick@hedonic-technologies.com>
2015-03-15 20:27:37 +0100

    sans le serveur

--- main.js
+++ main.js
@@ -32,36 +32,36 @@ simpleImap.start();
 
 
 
-
-var server = http.createServer(function(req, res) {
-    var page = url.parse(req.url).pathname;
-    console.log(page);
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    if (page == '/') {
-        res.write('Vous êtes à l\'accueil, que puis-je pour vous ?');
-    }
-    else if (page == '/on') {
-        res.write('on');
-        gpio.open(16, "output", function(err) {     // Open pin 16 for output
-            gpio.write(16, 1, function() {          // Set pin 16 high (1)
-                gpio.close(16);                     // Close pin 16
-            });
-        });
-    }
-    else if (page == '/off') {
-        res.write('off');
-        gpio.open(16, "output", function(err) {     // Open pin 16 for output
-            gpio.write(16, 0, function() {          // Set pin 16 high (1)
-                gpio.close(16);                     // Close pin 16
-            });
-        });
-    }
-    res.end();
-});
-
-function writetoconsole () {
-    console.log('coucou');
-}
-
-server.listen(5000);
+//
+//var server = http.createServer(function(req, res) {
+//    var page = url.parse(req.url).pathname;
+//    console.log(page);
+//    res.writeHead(200, {"Content-Type": "text/plain"});
+//    if (page == '/') {
+//        res.write('Vous êtes à l\'accueil, que puis-je pour vous ?');
+//    }
+//    else if (page == '/on') {
+//        res.write('on');
+//        gpio.open(16, "output", function(err) {     // Open pin 16 for output
+//            gpio.write(16, 1, function() {          // Set pin 16 high (1)
+//                gpio.close(16);                     // Close pin 16
+//            });
+//        });
+//    }
+//    else if (page == '/off') {
+//        res.write('off');
+//        gpio.open(16, "output", function(err) {     // Open pin 16 for output
+//            gpio.write(16, 0, function() {          // Set pin 16 high (1)
+//                gpio.close(16);                     // Close pin 16
+//            });
+//        });
+//    }
+//    res.end();
+//});
+//
+//function writetoconsole () {
+//    console.log('coucou');
+//}
+//
+//server.listen(5000);
 
